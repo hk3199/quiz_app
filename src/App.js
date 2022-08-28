419e78474bd3aa48bbfdd7b49f92a57f78ca4f22
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2022-08-28 18:57:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 
-import { useState } from 'react';
-import axios from 'axios';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
@@ -8,44 +6,23 @@ import Home from './Pages/Home/Home';
 import Quiz from './Pages/Quiz/Quiz';
 import Result from './Pages/Result/Result';
 function App() {
-  const [name,setName]=useState ()
-  const fetchQuestions=async(category="",difficulty="") => {             //api call
-      const{data}=await axios.get(  `https://opentdb.com/api.php?amount=10${
-        category && `&category=${category}`
-      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
-    );
-
-    setQuestions(data.results);
-          // console.log(data);
-  }
-  const [questions,setQuestions]=useState();
-  const [score,setScore]=useState(0);
-
   return (
-     <BrowserRouter>
+    <BrowserRouter>
     <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
       <Header/>
       <Routes>
-      <Route path="/" element={<Home 
-      name={name} 
-      setName={setName} 
-      fetchQuestions={fetchQuestions}/>} />
-
-      <Route  path="/quiz" element={<Quiz 
-      name={name}
-      questions={questions}
-      score={score}
-      setScore={setScore}
-      setQuestions={setQuestions}
-      />} />
-      <Route  path="/result" element={<Result
-      name={name}
-      score={score}
-      
-      />} />
+            {/* <Route path="/"element={<Home/>}/>
+           
+           
+            <Route path="/quiz" exact>
+              <Quiz></Quiz>
+            </Route>
+            <Route path='/result' exact>
+              <Result/>
+            </Route> */}
       </Routes>
       </div>
-     </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,32 +1,9 @@
-import { Button } from "@material-ui/core";
-import { useEffect } from "react";
-import {useNavigate } from "react-router-dom";
-import "./Result.css"
-const navigate=useNavigate
-const Result = ({name,score}) => {
-useEffect(()=>{
-  if(!name)
-  {
-    navigate("/");
-  }
-},[name]);
-
+const Result = () => {
     return (
-      <div className="result">
-       <span className="title">Final Score: {score}/10</span> 
-       <Button
-        variant="contained"
-        color="secondary"
-        size="large"
-        style={{ alignSelf: "center", marginTop: 20 }}
-        href="/"
-      >
-        Go to homepage
-      </Button>
+      <div >
+      result
+    
       </div>
-      // <Button>
-
-      // </Button>
     );
   };
   
