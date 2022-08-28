00fbb8ef6085682c680aabc8e799ab9944c5c6ff
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2022-08-28 18:35:28 +0530

    Changes to be committed: 	new file:   public/quiz.svg 	modified:   src/App.js 	new file:   src/Data/Categories.js 	modified:   src/Pages/Home/Home.css 	modified:   src/Pages/Home/Home.js 	modified:   src/Pages/Quiz/Quiz.css 	modified:   src/Pages/Quiz/Quiz.js 	modified:   src/Pages/Result/Result.css 	modified:   src/Pages/Result/Result.js 	new file:   src/components/ErrorMessage/ErrorMessage.js 	new file:   src/components/Question/Question.css 	new file:   src/components/Question/Question.js 	modified:   src/index.js

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,9 +1,32 @@
-const Result = () => {
+import { Button } from "@material-ui/core";
+import { useEffect } from "react";
+import {useNavigate } from "react-router-dom";
+import "./Result.css"
+const navigate=useNavigate
+const Result = ({name,score}) => {
+useEffect(()=>{
+  if(!name)
+  {
+    navigate("/");
+  }
+},[name]);
+
     return (
-      <div >
-      result
-    
+      <div className="result">
+       <span className="title">Final Score: {score}/10</span> 
+       <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        style={{ alignSelf: "center", marginTop: 20 }}
+        href="/"
+      >
+        Go to homepage
+      </Button>
       </div>
+      // <Button>
+
+      // </Button>
     );
   };
   
---
+++ src/components/Question/Question.js
@@ -0,0 +1,124 @@
+import { Button } from "@material-ui/core";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import ErrorMessage from "../ErrorMessage/ErrorMessage"
+import "./Question.css"
+// const Question = () =>{
+//     return(
+//         <div>quiz question</div>
+//     )
+// }
+const Question = ({
+        currQues,
+        setCurrQues,
+          questions,
+          options,
+          correct,
+          score,
+          setScore,
+          setQuestions}
+) => {
+       //selected blue state
+       const [selected,setSelected] = useState()
+        //use error message comp
+       const [error,setError]= useState(false)
+       const handleSelect = (i) =>{
+            if(selected===i && selected===correct)
+            {
+                return "select";
+                //return classname of select
+            }
+            else if(selected ===i && selected!==correct)
+            {
+                return "wrong";
+            }
+            else if(i===correct)
+            {
+                return "select";
+            }
+       };
+
+       const handleCheck= (i) =>{
+            setSelected(i);
+            //set the selected state to be i
+            if(i===correct) setScore(score+1);
+            setError(false);
+       };
+
+       const navigate= useNavigate();
+
+       const handleNext=()=>{
+        if(currQues>8)
+        {
+            navigate("/result");
+        }
+        else if(selected)
+        {
+            setCurrQues(currQues+1);
+            setSelected();
+        }
+        else
+        {
+            setError("Please select an option");
+        }
+       }
+
+       const handleQuit=()=>{
+        setCurrQues(0);
+        setQuestions();
+       }
+
+       return (<div className="question">
+        <h1>
+            Question {currQues+1} 
+        </h1>
+
+        <div className="singleQuestion">
+            <h2>{questions[currQues].question}</h2>
+
+            <div className="options">
+                    {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
+                    {/* check if something is inside ooptions */}
+
+                    {
+                        options &&
+                        options.map((i) => (
+                            <button
+                            className={`singleOption  ${selected && handleSelect(i)}`}
+                            key={i}
+                            onClick={() => handleCheck(i)}
+                            //check if selected answer is correct or not and makes the score go hugher
+                            disabled={selected}
+                            // disabled once selected
+                          >{i}</button>
+                        ))}
+                         </div>
+
+                            <div className="controls">
+                                <Button
+                                variant="contained"
+                                color="secondary"
+                                size="large"
+                                style={{ width: 185 }}
+                                href="/"
+                                onClick={handleQuit}
+                                >
+                                    Quit
+                                </Button>
+                                <Button
+                                variant="contained"
+                                color="secondary"
+                                size="large"
+                                style={{ width: 185 }}
+                                onClick={ handleNext}
+                                >
+                                    Next Question
+                                </Button>
+                            </div>
+
+        </div>
+       </div>
+       );  
+};
+
+export default Question
